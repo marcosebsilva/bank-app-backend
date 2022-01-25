4425e53d7d2109393cc2a61b4dc7af3586c52706
Marcos Estevao <dusdoom.dev@gmail.com>
2022-01-25 17:33:01 -0300

    updating service and models tests structure

--- tests/unit/models/users.models.test.js
+++ tests/unit/models/users.models.test.js
@@ -29,60 +29,61 @@ after(async () => {
   await memoryServer.stop();
   MongoClient.connect.restore();
 });
-
-describe('CREATE', async () => {
-  after(async () => {
-    const conn = await connection();
-    await conn.collection('users').drop();
-  });
-  const { VALID_CPF, VALID_NAME } = testValues;
-  it('returns the expected object', async () => {
-    const result = await usersModels.create(VALID_NAME, VALID_CPF);
-    expect(result).to.be.a('object');
-    expect(result).to.have.all.keys('acknowledged', 'insertedId');
-  });
-  it('creates the user in the DB', async () => {
-    const conn = await connection();
-    const query = await conn.collection('users').findOne({ 'account_owner.cpf': VALID_CPF });
-    expect(query).to.be.a('object')
-      .that.has.all.keys('_id', 'account_owner', 'credit');
-    expect(query).to.have.nested.property('account_owner.name');
-    expect(query).to.have.nested.property('account_owner.cpf');
-    expect(query.account_owner.cpf).to.be.equal(VALID_CPF);
-  });
-});
-
-describe('FIND BY CPF', async () => {
-  const { VALID_NAME, VALID_CPF } = testValues;
-  describe('if the user exists', async () => {
-    before(async () => {
-      const conn = await connection();
-      await conn.collection('users').insertOne({
-        account_owner: {
-          cpf: VALID_CPF,
-          name: VALID_NAME,
-        },
-        credit: 0,
-      });
-    });
+describe('MODELS', async () => {
+  describe('create()', async () => {
     after(async () => {
       const conn = await connection();
       await conn.collection('users').drop();
     });
-    it('should return the right user', async () => {
-      const result = await usersModels.findByCpf(VALID_CPF);
-      expect(result).to.be.a('object')
+    const { VALID_CPF, VALID_NAME } = testValues;
+    it('returns the expected object', async () => {
+      const result = await usersModels.create(VALID_NAME, VALID_CPF);
+      expect(result).to.be.a('object');
+      expect(result).to.have.all.keys('acknowledged', 'insertedId');
+    });
+    it('creates the user in the DB', async () => {
+      const conn = await connection();
+      const query = await conn.collection('users').findOne({ 'account_owner.cpf': VALID_CPF });
+      expect(query).to.be.a('object')
         .that.has.all.keys('_id', 'account_owner', 'credit');
-      expect(result).to.have.nested.property('account_owner.name');
-      expect(result).to.have.nested.property('account_owner.cpf');
-      expect(result.account_owner.cpf).to.be.equal(VALID_CPF);
+      expect(query).to.have.nested.property('account_owner.name');
+      expect(query).to.have.nested.property('account_owner.cpf');
+      expect(query.account_owner.cpf).to.be.equal(VALID_CPF);
     });
   });
-  describe('if the user does not exists', async () => {
-    it('should return nothing', async () => {
-      const result = await usersModels.findByCpf(VALID_CPF);
-      expect(result).to.not.be.a('object');
-      expect(result).to.be.a('null');
+
+  describe('findByCpf()', async () => {
+    const { VALID_NAME, VALID_CPF } = testValues;
+    describe('if the user exists', async () => {
+      before(async () => {
+        const conn = await connection();
+        await conn.collection('users').insertOne({
+          account_owner: {
+            cpf: VALID_CPF,
+            name: VALID_NAME,
+          },
+          credit: 0,
+        });
+      });
+      after(async () => {
+        const conn = await connection();
+        await conn.collection('users').drop();
+      });
+      it('should return the right user', async () => {
+        const result = await usersModels.findByCpf(VALID_CPF);
+        expect(result).to.be.a('object')
+          .that.has.all.keys('_id', 'account_owner', 'credit');
+        expect(result).to.have.nested.property('account_owner.name');
+        expect(result).to.have.nested.property('account_owner.cpf');
+        expect(result.account_owner.cpf).to.be.equal(VALID_CPF);
+      });
+    });
+    describe('if the user does not exists', async () => {
+      it('should return nothing', async () => {
+        const result = await usersModels.findByCpf(VALID_CPF);
+        expect(result).to.not.be.a('object');
+        expect(result).to.be.a('null');
+      });
     });
   });
 });
--- tests/unit/services/users.services.test.js
+++ tests/unit/services/users.services.test.js
@@ -14,74 +14,75 @@ const CustomError = require('../../../utils/CustomError');
 const statusCode = require('../../../utils/statusCode.json');
 
 use(chaiAsPromised);
-
-describe('VALIDATE BODY', async () => {
-  const {
-    INVALID_CPF,
-    INVALID_NAME,
-    VALID_CPF,
-    VALID_NAME,
-  } = testValues;
-  describe('If the body is wrong', async () => {
-    before(async () => {
-      sinon.stub(userModels, 'create').resolves({
-        acknowledged: null,
-        insertedId: null,
+describe('SERVICES', async () => {
+  describe('validateBody()', async () => {
+    const {
+      INVALID_CPF,
+      INVALID_NAME,
+      VALID_CPF,
+      VALID_NAME,
+    } = testValues;
+    describe('If the body is wrong', async () => {
+      before(async () => {
+        sinon.stub(userModels, 'create').resolves({
+          acknowledged: null,
+          insertedId: null,
+        });
+        sinon.stub(userModels, 'findByCpf').resolves({
+          _id: null,
+          account_owner: {
+            name: null,
+            cpf: null,
+          },
+          credit: null,
+        });
       });
-      sinon.stub(userModels, 'findByCpf').resolves({
-        _id: null,
-        account_owner: {
-          name: null,
-          cpf: null,
-        },
-        credit: null,
+      after(() => {
+        userModels.create.restore();
+        userModels.findByCpf.restore();
+      });
+
+      it('should throw an CustomError if there`s missing data', async () => {
+        await expect(userServices.validateBody({ cpf: undefined, name: VALID_NAME }))
+          .to.eventually.be.rejectedWith('Missing CPF or NAME field.')
+          .and.be.an.instanceOf(CustomError)
+          .and.have.property('status', statusCode.BAD_REQUEST);
+        await expect(userServices.validateBody({ cpf: VALID_CPF, name: undefined }))
+          .to.eventually.be.rejectedWith('Missing CPF or NAME field.')
+          .and.be.an.instanceOf(CustomError)
+          .and.have.property('status', statusCode.BAD_REQUEST);
+      });
+      it('should throw an CustomError if CPF is invalid', async () => {
+        await expect(userServices.validateBody({ cpf: INVALID_CPF, name: VALID_NAME }))
+          .to.eventually.be.rejectedWith('Invalid CPF format.')
+          .and.be.an.instanceOf(CustomError)
+          .and.have.property('status', statusCode.BAD_REQUEST);
+      });
+      it('should throw an CustomError if CPF is already registered', async () => {
+        await expect(userServices.validateBody({ cpf: VALID_CPF, name: VALID_NAME }))
+          .to.eventually.be.rejectedWith('User already registered.')
+          .and.be.an.instanceOf(CustomError)
+          .and.have.property('status', statusCode.ALREADY_REGISTERED);
+      });
+      it('should throw an CustomError if NAME is invalid', async () => {
+        await expect(userServices.validateBody({ cpf: VALID_CPF, name: INVALID_NAME }))
+          .to.eventually.be.rejectedWith('Invalid NAME format.')
+          .and.be.an.instanceOf(CustomError)
+          .and.have.property('status', statusCode.BAD_REQUEST);
       });
     });
-    after(() => {
-      userModels.create.restore();
-      userModels.findByCpf.restore();
-    });
-
-    it('should throw an CustomError if there`s missing data', async () => {
-      await expect(userServices.validateBody({ cpf: undefined, name: VALID_NAME }))
-        .to.eventually.be.rejectedWith('Missing CPF or NAME field.')
-        .and.be.an.instanceOf(CustomError)
-        .and.have.property('status', statusCode.BAD_REQUEST);
-      await expect(userServices.validateBody({ cpf: VALID_CPF, name: undefined }))
-        .to.eventually.be.rejectedWith('Missing CPF or NAME field.')
-        .and.be.an.instanceOf(CustomError)
-        .and.have.property('status', statusCode.BAD_REQUEST);
-    });
-    it('should throw an CustomError if CPF is invalid', async () => {
-      await expect(userServices.validateBody({ cpf: INVALID_CPF, name: VALID_NAME }))
-        .to.eventually.be.rejectedWith('Invalid CPF format.')
-        .and.be.an.instanceOf(CustomError)
-        .and.have.property('status', statusCode.BAD_REQUEST);
-    });
-    it('should throw an CustomError if CPF is already registered', async () => {
-      await expect(userServices.validateBody({ cpf: VALID_CPF, name: VALID_NAME }))
-        .to.eventually.be.rejectedWith('User already registered.')
-        .and.be.an.instanceOf(CustomError)
-        .and.have.property('status', statusCode.ALREADY_REGISTERED);
-    });
-    it('should throw an CustomError if NAME is invalid', async () => {
-      await expect(userServices.validateBody({ cpf: VALID_CPF, name: INVALID_NAME }))
-        .to.eventually.be.rejectedWith('Invalid NAME format.')
-        .and.be.an.instanceOf(CustomError)
-        .and.have.property('status', statusCode.BAD_REQUEST);
-    });
-  });
-  describe('If the body is right', async () => {
-    before(async () => {
-      sinon.stub(userModels, 'findByCpf').resolves(null);
-    });
-    after(async () => {
-      userModels.findByCpf.restore();
-    });
-    it('should return nothing', async () => {
-      const result = await userServices.validateBody({ cpf: VALID_CPF, name: VALID_NAME });
-      // eslint-disable-next-line no-unused-expressions
-      expect(result).to.be.undefined;
+    describe('If the body is right', async () => {
+      before(async () => {
+        sinon.stub(userModels, 'findByCpf').resolves(null);
+      });
+      after(async () => {
+        userModels.findByCpf.restore();
+      });
+      it('should return nothing', async () => {
+        const result = await userServices.validateBody({ cpf: VALID_CPF, name: VALID_NAME });
+        // eslint-disable-next-line no-unused-expressions
+        expect(result).to.be.undefined;
+      });
     });
   });
 });
