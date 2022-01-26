2ca7c1a81ce8b0da7a1e72e29a7c989bc0ff9e00
Marcos Estevao <dusdoom.dev@gmail.com>
2022-01-26 20:58:44 -0300

    [FEATURE] [:white_check_mark: TESTED] userServices.login()

--- services/userServices.js
+++ services/userServices.js
@@ -2,16 +2,33 @@ const bcrypt = require('bcrypt');
 const userModels = require('../models/userModels');
 const CustomError = require('../utils/CustomError');
 const serviceHelpers = require('./serviceHelpers');
+const statusCode = require('../utils/statusCode.json');
+const authService = require('./authService');
 
 const create = async (body) => {
   const bodyIsValid = serviceHelpers.validateRegisterBody(body);
   if (bodyIsValid !== true) throw bodyIsValid;
-  const { name, cpf, password } = body;
-  const alreadyRegistered = await userModels.findByCpf(cpf);
-  if (alreadyRegistered) throw new CustomError('User already registered.', 409);
-  const hashedPassword = await bcrypt.hash(password, 10);
-  await userModels.create(name, cpf, hashedPassword);
+  const foundUser = await userModels.findByCpf(body.cpf);
+  if (foundUser) throw new CustomError('User already registered.', 409);
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+  await userModels.create(body.name, body.cpf, hashedPassword);
+};
+
+const login = async (body) => {
+  const bodyIsValid = serviceHelpers.validateLoginBody(body);
+  if (bodyIsValid !== true) throw bodyIsValid;
+  const foundUser = await userModels.findByCpf(body.cpf);
+  if (!foundUser) throw new CustomError('User not found.', statusCode.NOT_FOUND);
+  const checkPassword = await bcrypt.compare(body.password, foundUser.password);
+  if (!checkPassword) throw new CustomError('Wrong password.', statusCode.UNAUTHORIZED);
+
+  const { password, ...verifiedUser } = foundUser;
+
+  const accessToken = authService.generateToken(verifiedUser);
+
+  return accessToken;
 };
 module.exports = {
   create,
+  login,
 };
