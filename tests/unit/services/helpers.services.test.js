3865f1f604cbe6b7be0e83197d4d28df6b14ba4a
Marcos Estevao <dusdoom.dev@gmail.com>
2022-01-26 15:04:05 -0300

    removing .only from test

--- tests/unit/services/helpers.services.test.js
+++ tests/unit/services/helpers.services.test.js
@@ -14,35 +14,62 @@ const {
   INVALID_NAME,
   VALID_CPF,
   VALID_NAME,
+  VALID_PASSWORD,
+  INVALID_PASSWORD,
 } = testValues;
 
 describe('HELPERS', async () => {
   describe('validateBody()', async () => {
-    it('should throw an CustomError if there`s missing data', async () => {
-      let result = serviceHelpers.validateBody({ cpf: undefined, name: VALID_NAME });
+    it('should return an CustomError if there`s missing data', async () => {
+      let result = serviceHelpers.validateBody(
+        { cpf: undefined, name: VALID_NAME, password: VALID_PASSWORD },
+      );
       expect(result).to.be.an.instanceOf(CustomError);
       expect(result).to.have.property('status', statusCode.BAD_REQUEST);
-      expect(result).to.have.property('message', 'Missing CPF or NAME field.');
+      expect(result).to.have.property('message', 'All fields are required.');
 
-      result = serviceHelpers.validateBody({ cpf: VALID_CPF, name: undefined });
+      result = serviceHelpers.validateBody(
+        { cpf: VALID_CPF, name: undefined, password: VALID_PASSWORD },
+      );
       expect(result).to.be.an.instanceOf(CustomError);
       expect(result).to.have.property('status', statusCode.BAD_REQUEST);
-      expect(result).to.have.property('message', 'Missing CPF or NAME field.');
+      expect(result).to.have.property('message', 'All fields are required.');
+
+      result = serviceHelpers.validateBody(
+        { cpf: VALID_CPF, name: VALID_NAME, password: undefined },
+      );
+      expect(result).to.be.an.instanceOf(CustomError);
+      expect(result).to.have.property('status', statusCode.BAD_REQUEST);
+      expect(result).to.have.property('message', 'All fields are required.');
     });
-    it('should throw an CustomError if CPF is invalid', async () => {
-      const result = serviceHelpers.validateBody({ cpf: INVALID_CPF, name: VALID_NAME });
+    it('should return an CustomError if CPF is invalid', async () => {
+      const result = serviceHelpers.validateBody(
+        { cpf: INVALID_CPF, name: VALID_NAME, password: VALID_PASSWORD },
+      );
       expect(result).to.be.an.instanceOf(CustomError);
       expect(result).to.have.property('status', statusCode.BAD_REQUEST);
       expect(result).to.have.property('message', 'Invalid CPF format.');
     });
-    it('should throw an CustomError if NAME is invalid', async () => {
-      const result = serviceHelpers.validateBody({ cpf: VALID_CPF, name: INVALID_NAME });
+    it('should return an CustomError if NAME is invalid', async () => {
+      const result = serviceHelpers.validateBody(
+        { cpf: VALID_CPF, name: INVALID_NAME, password: VALID_PASSWORD },
+      );
       expect(result).to.be.an.instanceOf(CustomError);
       expect(result).to.have.property('status', statusCode.BAD_REQUEST);
       expect(result).to.have.property('message', 'Invalid NAME format.');
     });
+    it('should return an Custom if the PASSWORD is invalid', async () => {
+      const result = serviceHelpers.validateBody(
+        { cpf: VALID_CPF, name: VALID_NAME, password: INVALID_PASSWORD },
+      );
+      expect(result).to.be.an.instanceOf(CustomError);
+      expect(result).to.have.property('status', statusCode.BAD_REQUEST);
+      expect(result).to.have.property('message', 'Invalid PASSWORD format.');
+    });
     it('should return nothing everything is fine', async () => {
-      const result = await serviceHelpers.validateBody({ cpf: VALID_CPF, name: VALID_NAME });
+      const result = await serviceHelpers.validateBody(
+        { cpf: VALID_CPF, name: VALID_NAME, password: VALID_PASSWORD },
+      );
       expect(result).to.be.equal(true);
     });
   });
