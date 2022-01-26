b08b9dc093035f9b1122747e7bd150c700fc2a6f
Marcos Estevao <dusdoom.dev@gmail.com>
2022-01-25 22:26:48 -0300

    simplified helpers tests

--- tests/unit/services/helpers.services.test.js
+++ tests/unit/services/helpers.services.test.js
@@ -3,13 +3,10 @@ const sinon = require('sinon');
 const {
   describe,
   it,
-  before,
-  after,
 } = require('mocha');
 const { expect } = require('chai');
 const serviceHelpers = require('../../../services/serviceHelpers');
 const testValues = require('../utils/testValues.json');
-const userModels = require('../../../models/userModels');
 const CustomError = require('../../../utils/CustomError');
 const statusCode = require('../../../utils/statusCode.json');
 
@@ -22,41 +19,32 @@ const {
 
 describe('HELPERS', async () => {
   describe('validateBody()', async () => {
-    describe('If the body is wrong', async () => {
-      it('should throw an CustomError if there`s missing data', async () => {
-        expect(() => serviceHelpers.validateBody({ cpf: undefined, name: VALID_NAME }))
-          .to.throw('Missing CPF or NAME field.')
-          .and.be.an.instanceOf(CustomError)
-          .and.have.property('status', statusCode.BAD_REQUEST);
-        expect(() => serviceHelpers.validateBody({ cpf: VALID_CPF, name: undefined }))
-          .to.throw('Missing CPF or NAME field.')
-          .and.be.an.instanceOf(CustomError)
-          .and.have.property('status', statusCode.BAD_REQUEST);
-      });
-      it('should throw an CustomError if CPF is invalid', async () => {
-        expect(() => serviceHelpers.validateBody({ cpf: INVALID_CPF, name: VALID_NAME }))
-          .to.throw('Invalid CPF format.')
-          .and.be.an.instanceOf(CustomError)
-          .and.have.property('status', statusCode.BAD_REQUEST);
-      });
-      it('should throw an CustomError if NAME is invalid', async () => {
-        expect(() => serviceHelpers.validateBody({ cpf: VALID_CPF, name: INVALID_NAME }))
-          .to.throw('Invalid NAME format.')
-          .and.be.an.instanceOf(CustomError)
-          .and.have.property('status', statusCode.BAD_REQUEST);
-      });
+    it('should throw an CustomError if there`s missing data', async () => {
+      let result = serviceHelpers.validateBody({ cpf: undefined, name: VALID_NAME });
+      expect(result).to.be.an.instanceOf(CustomError);
+      expect(result).to.have.property('status', statusCode.BAD_REQUEST);
+      expect(result).to.have.property('message', 'Missing CPF or NAME field.');
+
+      result = serviceHelpers.validateBody({ cpf: VALID_CPF, name: undefined });
+      expect(result).to.be.an.instanceOf(CustomError);
+      expect(result).to.have.property('status', statusCode.BAD_REQUEST);
+      expect(result).to.have.property('message', 'Missing CPF or NAME field.');
     });
-    describe('If the body is right', async () => {
-      before(async () => {
-        sinon.stub(userModels, 'findByCpf').resolves(null);
-      });
-      after(async () => {
-        userModels.findByCpf.restore();
-      });
-      it('should return nothing', async () => {
-        const result = await serviceHelpers.validateBody({ cpf: VALID_CPF, name: VALID_NAME });
-        expect(result).to.be.undefined;
-      });
+    it('should throw an CustomError if CPF is invalid', async () => {
+      const result = serviceHelpers.validateBody({ cpf: INVALID_CPF, name: VALID_NAME });
+      expect(result).to.be.an.instanceOf(CustomError);
+      expect(result).to.have.property('status', statusCode.BAD_REQUEST);
+      expect(result).to.have.property('message', 'Invalid CPF format.');
+    });
+    it('should throw an CustomError if NAME is invalid', async () => {
+      const result = serviceHelpers.validateBody({ cpf: VALID_CPF, name: INVALID_NAME });
+      expect(result).to.be.an.instanceOf(CustomError);
+      expect(result).to.have.property('status', statusCode.BAD_REQUEST);
+      expect(result).to.have.property('message', 'Invalid NAME format.');
+    });
+    it('should return nothing everything is fine', async () => {
+      const result = await serviceHelpers.validateBody({ cpf: VALID_CPF, name: VALID_NAME });
+      expect(result).to.be.equal(true);
     });
   });
 });
