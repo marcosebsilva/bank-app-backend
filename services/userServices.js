fb132c0bbb06bc61caae79b650db06d5c4bc4d8a
Marcos Estevao <dusdoom.dev@gmail.com>
2022-01-25 21:15:36 -0300

    updated validateBody() service

--- services/userServices.js
+++ services/userServices.js
@@ -1,12 +1,9 @@
 const CustomError = require('../utils/CustomError');
 const schemas = require('../utils/joi-schemas');
 const statusCode = require('../utils/statusCode.json');
-const userModels = require('../models/userModels');
+// const userModels = require('../models/userModels');
 
-const create = async () => {
-
-};
-const validateBody = async (body) => {
+const validateBody = (body) => {
   const { error } = schemas.newUserBody.validate(body);
   if (error) throw new CustomError('Missing CPF or NAME field.', statusCode.BAD_REQUEST);
   const { name, cpf } = body;
@@ -14,11 +11,15 @@ const validateBody = async (body) => {
   // thanks to Roko in https://stackoverflow.com/questions/64313880/regular-expression-to-match-exact-11-digits-numbers?noredirect=1&lq=1
   if (!validCPF.test(cpf)) throw new CustomError('Invalid CPF format.', statusCode.BAD_REQUEST);
   if (name.length <= 3) throw new CustomError('Invalid NAME format.', statusCode.BAD_REQUEST);
-
-  const alreadyExists = await userModels.findByCpf(cpf);
-  if (alreadyExists) throw new CustomError('User already registered.', statusCode.ALREADY_REGISTERED);
 };
+
+// const create = async (body) => {
+//   validateBody(body);
+//   const { name, cpf } = body;
+//   await userModels.create(name, cpf);
+//   return 'aaaaaaaaaaaaa';
+// };
 module.exports = {
-  create,
+  // create,
   validateBody,
 };
--- tests/unit/services/users.services.test.js
+++ tests/unit/services/users.services.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 const sinon = require('sinon');
 const {
   describe,
@@ -16,61 +17,33 @@ use(require('chai-as-promised'));
 use(require('sinon-chai'));
 
 describe('SERVICES', async () => {
+  const {
+    INVALID_CPF,
+    INVALID_NAME,
+    VALID_CPF,
+    VALID_NAME,
+  } = testValues;
   describe('validateBody()', async () => {
-    const {
-      INVALID_CPF,
-      INVALID_NAME,
-      VALID_CPF,
-      VALID_NAME,
-    } = testValues;
     describe('If the body is wrong', async () => {
-      before(async () => {
-        sinon.stub(userModels, 'create').resolves({
-          acknowledged: null,
-          insertedId: null,
-        });
-        sinon.stub(userModels, 'findByCpf').resolves({
-          _id: null,
-          account_owner: {
-            name: null,
-            cpf: null,
-          },
-          credit: null,
-        });
-      });
-      after(() => {
-        userModels.create.restore();
-        userModels.findByCpf.restore();
-      });
-
       it('should throw an CustomError if there`s missing data', async () => {
-        await expect(userServices.validateBody({ cpf: undefined, name: VALID_NAME }))
-          .to.eventually.be.rejectedWith('Missing CPF or NAME field.')
+        expect(() => userServices.validateBody({ cpf: undefined, name: VALID_NAME }))
+          .to.throw('Missing CPF or NAME field.')
           .and.be.an.instanceOf(CustomError)
           .and.have.property('status', statusCode.BAD_REQUEST);
-        await expect(userServices.validateBody({ cpf: VALID_CPF, name: undefined }))
-          .to.eventually.be.rejectedWith('Missing CPF or NAME field.')
+        expect(() => userServices.validateBody({ cpf: VALID_CPF, name: undefined }))
+          .to.throw('Missing CPF or NAME field.')
           .and.be.an.instanceOf(CustomError)
           .and.have.property('status', statusCode.BAD_REQUEST);
       });
       it('should throw an CustomError if CPF is invalid', async () => {
-        await expect(userServices.validateBody({ cpf: INVALID_CPF, name: VALID_NAME }))
-          .to.eventually.be.rejectedWith('Invalid CPF format.')
+        expect(() => userServices.validateBody({ cpf: INVALID_CPF, name: VALID_NAME }))
+          .to.throw('Invalid CPF format.')
           .and.be.an.instanceOf(CustomError)
           .and.have.property('status', statusCode.BAD_REQUEST);
       });
-      it('should throw an CustomError if CPF is already registered', async () => {
-        await expect(userServices.validateBody({ cpf: VALID_CPF, name: VALID_NAME }))
-          .to.eventually.be.rejectedWith('User already registered.')
-          .and.be.an.instanceOf(CustomError)
-          .and.have.property('status', statusCode.ALREADY_REGISTERED)
-          .then(() => {
-            expect(userModels.findByCpf.calledWith(VALID_CPF)).to.be.equal(true);
-          });
-      });
       it('should throw an CustomError if NAME is invalid', async () => {
-        await expect(userServices.validateBody({ cpf: VALID_CPF, name: INVALID_NAME }))
-          .to.eventually.be.rejectedWith('Invalid NAME format.')
+        expect(() => userServices.validateBody({ cpf: VALID_CPF, name: INVALID_NAME }))
+          .to.throw('Invalid NAME format.')
           .and.be.an.instanceOf(CustomError)
           .and.have.property('status', statusCode.BAD_REQUEST);
       });
