0570f54f60e5c6cfb07c0ab46ea89f646bcec48b
Marcos Estevao <dusdoom.dev@gmail.com>
2022-01-26 21:48:53 -0300

    [TESTS] user.controllers.test.js

--- tests/unit/controllers/users.controllers.test.js
+++ tests/unit/controllers/users.controllers.test.js
@@ -11,6 +11,7 @@ const { expect, use } = require('chai');
 const userServices = require('../../../services/userServices');
 const userControllers = require('../../../controllers/userController');
 const statusCode = require('../../../utils/statusCode.json');
+const testValues = require('../utils/testValues');
 
 use(require('sinon-chai'));
 
@@ -32,7 +33,6 @@ describe('CONTROLLERS', async () => {
   describe('create()', async () => {
     describe('When the request is malformed', async () => {
       before(() => {
-        request.body = {};
         sinon.stub(userServices, 'create').rejects();
       });
       after(() => {
@@ -43,7 +43,7 @@ describe('CONTROLLERS', async () => {
         expect(next).to.have.been.called;
       });
     });
-    describe('if the body is right', () => {
+    describe('When the request is right', () => {
       before(() => {
         sinon.stub(userServices, 'create').resolves({});
       });
@@ -53,6 +53,36 @@ describe('CONTROLLERS', async () => {
       it('should return status 201', async () => {
         await userControllers.create(request, response, next);
         expect(response.sendStatus).to.have.been.calledWith(statusCode.CREATED);
+      });
+    });
+  });
+  describe('login()', async () => {
+    describe('When the request is malformed', async () => {
+      before(() => {
+        sinon.stub(userServices, 'login').rejects();
+      });
+      after(() => {
+        userServices.login.restore();
+      });
+      it('should call error middleware', async () => {
+        await userControllers.login(request, response, next);
+        expect(next).to.have.been.called;
+      });
+    });
+    describe('When the request is right', async () => {
+      before(() => {
+        sinon.stub(userServices, 'login').resolves(testValues.EXAMPLE_TOKEN);
+      });
+      after(() => {
+        userServices.login.restore();
+      });
+      it('should return status 200', async () => {
+        await userControllers.login(request, response, next);
+        expect(response.status).to.have.been.calledWith(statusCode.OK);
+      });
+      it('should return a jwt token', async () => {
+        await userControllers.login(request, response, next);
+        expect(response.json).to.have.been.calledWith({ token: testValues.EXAMPLE_TOKEN });
       });
     });
   });
